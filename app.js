476968c481651665c3c548087e6d1ab81e74bc1c
Dev Lodha <lodhad@gmail.com>
2017-04-17 23:37:04 -0700

    Cleaned up a bit

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var stylus = require('stylus');
+var nib = require('nib');
+
 var environment = require('./env');
 
 var mongo = require('mongodb');
@@ -16,23 +19,32 @@ var users = require('./routes/users');
 
 var app = express();
 
+var compileStylus = function compile(str, path) {
+  return stylus(str).set('filename', path).use(nib());
+};
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//Middleware setuip
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(stylus.middleware({
+  src: __dirname + '/public',
+  compile: compileStylus
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(function(req, res, next) {
 	req.db = db;
 	next();
 });
 
+//Route setup
 app.use('/', index);
 app.use('/users', users);
 
--- routes/index.js
+++ routes/index.js
@@ -3,44 +3,44 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {});
 });
 
-router.get('/helloworld', function(req, res) {
-	res.render("helloworld", {title: 'Hello World!'});
-});
+// router.get('/helloworld', function(req, res) {
+// 	res.render("helloworld", {title: 'Hello World!'});
+// });
 
-router.get('/userlist', function(req, res) {
-	var db = req.db;
-	var collection = db.get('usercollection');
-	collection.find({}, {}, function(e, found) {
-		res.render('userlist', {
-			"userlist": found
-		});
-	});
-});
+// router.get('/userlist', function(req, res) {
+// 	var db = req.db;
+// 	var collection = db.get('usercollection');
+// 	collection.find({}, {}, function(e, found) {
+// 		res.render('userlist', {
+// 			"userlist": found
+// 		});
+// 	});
+// });
 
-router.get("/newuser", function(req, res) {
-	res.render('newuser', {title: "Add new user"});
-});
+// router.get("/newuser", function(req, res) {
+// 	res.render('newuser', {title: "Add new user"});
+// });
 
-router.post("/adduser", function(req, res) {
-	var db = req.db;
-	var name = req.body.username;
-	var email = req.body.useremail;
+// router.post("/adduser", function(req, res) {
+// 	var db = req.db;
+// 	var name = req.body.username;
+// 	var email = req.body.useremail;
 
-	var collection = db.get('usercollection');
+// 	var collection = db.get('usercollection');
 
-	collection.insert({
-		'username': name,
-		'email': email
-	}, function(err, result) {
-		if(err) {
-			res.send("Error!");
-		} else {
-			res.redirect("newuser");
-		}
-	});
-});
+// 	collection.insert({
+// 		'username': name,
+// 		'email': email
+// 	}, function(err, result) {
+// 		if(err) {
+// 			res.send("Error!");
+// 		} else {
+// 			res.redirect("newuser");
+// 		}
+// 	});
+// });
 
 module.exports = router;
