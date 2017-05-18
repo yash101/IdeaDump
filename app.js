150bf5f2e68bcd7c2eb8fa64f7966d4e27e3c90d
Dev Lodha <lodhad@gmail.com>
2017-05-17 20:59:01 -0700

    things run for once... :)

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Express stuff
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,6 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//Environment variables
 var env = require('./env');
 
 //Used by Passport
@@ -13,9 +15,22 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var passportLocalStrategy = require('passport-local').Strategy;
 
+//Used by Stylus
+var stylus = require('stylus');
+var nib = require('nib');
+//Compiles a Stylus file
+var compileStylus = function compile(str, path) {
+	return stylus(str).set('filename', path).use(nib());
+};
+
+//Used by JSX
+var jsxMiddleware = require('jsx-middleware');
+
+//Routers
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+//App
 var app = express();
 
 // view engine setup
@@ -25,6 +40,7 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -35,9 +51,18 @@ app.use(passport.session());
 app.use('/', index);
 app.use('/users', users);
 
+//Stylus middleware
+app.use(stylus.middleware({
+	src: __dirname + '/public',
+	compile: compileStylus
+}));
+//JSx middleware
+app.use(jsxMiddleware(__dirname + '/public'));
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 var Account = require('./models/userAccount');
+
 passport.use(new passportLocalStrategy(Account.authenticate()));
 passport.serializeUser(Account.serializeUser());
 passport.deserializeUser(Account.deserializeUser());
